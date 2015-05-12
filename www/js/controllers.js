ca47af7b999bc61ebb819c20e7cb21940cbb8a8a
Casey Klimkowsky <cklimkowsky@gmail.com>
2015-05-11 21:01:58 -0400

    Added Moment.js to compute time from now, and added color labels for classes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,10 @@ angular.module('homework.controllers', [])
 
 .controller('ClassDetailCtrl', function($scope, $stateParams, Classes) {
 	$scope.class = Classes.get($stateParams.classId);
+
+	$scope.getNextMeetingTime = function() {
+		// TODO: Use Moment.js to return a string containing the next meeting time of the class
+	};
 })
 
 .controller('CalendarCtrl', function($scope) {
