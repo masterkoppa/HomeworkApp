dc280348759f5703f979dd0ef8457f3a50fdb53b
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2015-05-11 23:46:47 -0400

    Change classlist service to use local storage
    Right now it fills in the data with the test data, but
    will load from local storage from there on.

--- www/js/services.js
+++ www/js/services.js
@@ -3,65 +3,78 @@ angular.module('homework.services', [])
 .factory('Classes', function() {
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var classes = [{
-    id: 0,
-    name: 'Class #1',
-    teacher: 'John Doe',
-    location: 'GOL-3123',
-    assignments: [{
+  var class_id = 0;
+  var class_list = {}
+  console.log("Got here");
+  // If class list does not exist in storage
+  if(!localStorage.class_list){
+    // Some fake testing data
+    var classes = [{
       id: 0,
-      name: 'Assignment #1',
-      classId: 0,
-      dueDate: new Date(2015, 4, 13, 23, 59)
+      name: 'Class #1',
+      teacher: 'John Doe',
+      location: 'GOL-3123',
+      assignments: [{
+        id: 0,
+        name: 'Assignment #1',
+        classId: 0,
+        dueDate: new Date(2015, 4, 13, 23, 59)
+      }, {
+        id: 3,
+        name: 'Assignment #4',
+        teacher: 'John Doe',
+        classId: 0,
+        dueDate: new Date(2015, 4, 13, 23, 59)
+      }]
+    }, {
+      id: 1,
+      name: 'Class #2',
+      teacher: 'John Doe',
+      location: 'GOL-3123',
+      assignments: [{
+        id: 1,
+        name: 'Assignment #2',
+        classId: 1,
+        dueDate: new Date(2015, 4, 13, 23, 59)
+      }]
+    }, {
+      id: 2,
+      name: 'Class #3',
+      teacher: 'John Doe',
+      location: 'GOL-3123',
+      assignments: [{
+        id: 3,
+        name: 'Assignment #3',
+        classId: 2,
+        dueDate: new Date(2015, 4, 13, 23, 59)
+      }]
     }, {
       id: 3,
-      name: 'Assignment #4',
+      name: 'Class #4',
       teacher: 'John Doe',
-      classId: 0,
-      dueDate: new Date(2015, 4, 13, 23, 59)
-    }]
-  }, {
-    id: 1,
-    name: 'Class #2',
-    teacher: 'John Doe',
-    location: 'GOL-3123',
-    assignments: [{
-      id: 1,
-      name: 'Assignment #2',
-      classId: 1,
-      dueDate: new Date(2015, 4, 13, 23, 59)
-    }]
-  }, {
-    id: 2,
-    name: 'Class #3',
-    teacher: 'John Doe',
-    location: 'GOL-3123',
-    assignments: [{
-      id: 3,
-      name: 'Assignment #3',
-      classId: 2,
-      dueDate: new Date(2015, 4, 13, 23, 59)
-    }]
-  }, {
-    id: 3,
-    name: 'Class #4',
-    teacher: 'John Doe',
-    location: 'GOL-3123',
-    assignments: []
-  }];
+      location: 'GOL-3123',
+      assignments: []
+    }];
+
+    localStorage.class_list = JSON.stringify(classes);
+  }
+
+  
+  class_list = JSON.parse(localStorage.class_list);
+
+  
 
   return {
     all: function() {
-      return classes;
+      return class_list;
     },
     remove: function(classObject) {
-      classes.splice(classes.indexOf(classObject), 1);
+      class_list.splice(class_list.indexOf(classObject), 1);
     },
     get: function(classId) {
-      for (var i = 0; i < classes.length; i++) {
-        if (classes[i].id === parseInt(classId)) {
-          return classes[i];
+      for (var i = 0; i < class_list.length; i++) {
+        if (class_list[i].id === parseInt(classId)) {
+          return class_list[i];
         }
       }
       return null;
