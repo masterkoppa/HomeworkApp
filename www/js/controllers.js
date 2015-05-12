cc378d8dfc71fdeec61a00cd2c6a023247e7b2b8
Casey Klimkowsky <cklimkowsky@gmail.com>
2015-05-12 13:07:36 -0400

    Added color labels to classes on Home screen, and added relative dates for class meeting times and assignment due dates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,14 +2,34 @@ angular.module('homework.controllers', [])
 
 .controller('HomeCtrl', function($scope, Classes) {
 	$scope.classes = Classes.all();
+
+	$scope.getNextMeetingTime = function(classObject) {
+		for (var i = 0; i < classObject.meetingTimes.length; i++) {
+			var meetingTime = classObject.meetingTimes[i];
+
+			if (meetingTime.day == moment().day() && meetingTime.end.hour > moment().hour() && meetingTime.end.minute > moment().minute() && meetingTime.start.hour <= moment().hour() && meetingTime.start.minute <= moment().minute()) {
+				return 'Meeting Now';
+			} else if (meetingTime.day == moment().day() && meetingTime.start.hour > moment().hour()) {
+				if (meetingTime.start.hour > moment().hour()) {
+					return 'Meets in ' + (meetingTime.start.hour - moment().hour() - 1) + ' hours ' + ((60 + meetingTime.start.minute) - moment().minute()) + ' minutes';
+				} else {
+					return 'Meets in ' + (meetingTime.start.minute - moment().minute()) + ' minutes';
+				}
+			} else if (meetingTime.day > moment().day()) {
+				if (meetingTime.day - moment().day() == 1) {
+					return 'Meets Tomorrow';
+				} else {
+					return 'Meets in ' + (meetingTime.day - moment().day()) + ' days';
+				}
+			} else if (i == classObject.meetingTimes.length - 1) {
+				return 'Meets in ' + (meetingTime.day + 7 - moment().day()) + ' days';
+			}
+		};
+	};
 })
 
 .controller('ClassDetailCtrl', function($scope, $stateParams, Classes) {
 	$scope.class = Classes.get($stateParams.classId);
-
-	$scope.getNextMeetingTime = function() {
-		// TODO: Use Moment.js to return a string containing the next meeting time of the class
-	};
 })
 
 .controller('CalendarCtrl', function($scope) {
@@ -18,6 +38,15 @@ angular.module('homework.controllers', [])
 
 .controller('AssignmentsCtrl', function($scope, Assignments) {
 	$scope.assignments = Assignments.all();
+
+	$scope.getTimeUntilDue = function(assignment) {
+		var now = new Date();
+
+		var start = moment([now.getYear(), now.getMonth(), now.getDate()]);
+		var end = moment([assignment.dueDate.getYear(), assignment.dueDate.getMonth(), assignment.dueDate.getDate()]);
+
+		return 'Due ' + end.from(start);
+	};
 })
 
 .controller('AssignmentDetailCtrl', function($scope, $stateParams, Assignments) {
--- www/js/services.js
+++ www/js/services.js
@@ -5,64 +5,148 @@ angular.module('homework.services', [])
 
   var class_id = 0;
   var class_list = {}
-  console.log("Got here");
-  // If class list does not exist in storage
-  if(!localStorage.class_list){
+
+  // If the class list does not exist in storage
+  if (!localStorage.class_list) {
     // Some fake testing data
     var classes = [{
       id: 0,
       name: 'Class #1',
       teacher: 'John Doe',
-      location: 'GOL-3123',
+      location: 'GOL-3123', 
+      color: 'SpringGreen',
+      meetingTimes: [{
+        day: 2,
+        start: {
+          hour: 11,
+          minute: 0
+        },
+        end: {
+          hour: 12,
+          minute: 15
+        }
+      }, {
+        day: 4,
+        start: {
+          hour: 11,
+          minute: 0
+        },
+        end: {
+          hour: 12,
+          minute: 15
+        }
+      }],
       assignments: [{
         id: 0,
         name: 'Assignment #1',
         classId: 0,
-        dueDate: new Date(2015, 4, 13, 23, 59)
+        dueDate: new Date(2015, 4, 19, 23, 59)
       }, {
         id: 3,
         name: 'Assignment #4',
         teacher: 'John Doe',
         classId: 0,
-        dueDate: new Date(2015, 4, 13, 23, 59)
+        dueDate: new Date(2015, 4, 19, 23, 59)
       }]
     }, {
       id: 1,
       name: 'Class #2',
       teacher: 'John Doe',
       location: 'GOL-3123',
+      color: 'SlateBlue',
+      meetingTimes: [{
+        day: 2,
+        start: {
+          hour:  17,
+          minute: 0
+        },
+        end: {
+          hour: 18,
+          minute: 15
+        }
+      }, {
+        day: 4,
+        start: {
+          hour: 17,
+          minute: 0
+        },
+        end: {
+          hour: 18,
+          minute: 15
+        }
+      }],
       assignments: [{
         id: 1,
         name: 'Assignment #2',
         classId: 1,
-        dueDate: new Date(2015, 4, 13, 23, 59)
+        dueDate: new Date(2015, 4, 19, 23, 59)
       }]
     }, {
       id: 2,
       name: 'Class #3',
       teacher: 'John Doe',
       location: 'GOL-3123',
+      color: 'SkyBlue',
+      meetingTimes: [{
+        day: 1,
+        start: {
+          hour: 13,
+          minute: 0
+        },
+        end: {
+          hour: 13,
+          minute: 50
+        }
+      }, {
+        day: 3,
+        start: {
+          hour: 13,
+          minute: 0
+        },
+        end: {
+          hour: 13,
+          minute: 50
+        }
+      }, {
+        day: 5,
+        start: {
+          hour: 13,
+          minute: 0
+        },
+        end: {
+          hour: 13,
+          minute: 50
+        }
+      }],
       assignments: [{
         id: 3,
         name: 'Assignment #3',
         classId: 2,
-        dueDate: new Date(2015, 4, 13, 23, 59)
+        dueDate: new Date(2015, 4, 19, 23, 59)
       }]
     }, {
       id: 3,
       name: 'Class #4',
       teacher: 'John Doe',
+      color: 'Orchid',
       location: 'GOL-3123',
+      meetingTimes: [{
+        day: 1,
+        start: {
+          hour: 2,
+          minute: 0
+        },
+        end: {
+          hour: 3,
+          minute: 50
+        }
+      }],
       assignments: []
     }];
 
     localStorage.class_list = JSON.stringify(classes);
   }
-
-  
   class_list = JSON.parse(localStorage.class_list);
-
-  
 
   return {
     all: function() {
